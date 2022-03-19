36c0ecf4349e5117e4b40b85fc154d3074ee0456
Shama Ugale <shama.ugale@gmail.com>
2022-03-20 02:29:38 +0530

    added more on binary search

--- src/com/shama/algorithms/binarySearch/FindFirstOccurrenceOfOne.java
+++ src/com/shama/algorithms/binarySearch/FindFirstOccurrenceOfOne.java
@@ -14,4 +14,39 @@ public class FindFirstOccurrenceOfOne {
 
      */
 
+    public static void main(String[] args) {
+
+        // lets say this is infinite sorted binary arr
+        int[] arr = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+        int start = 0;
+        int end = 1;
+        while(arr[end] != 1){
+            start = end;
+            end = end * 2;
+        }
+        int fistPositionForOne = findFirstPosition(arr, start, end, 1);
+        System.out.println("The 1st position of 1 in sorted binary array is : " + fistPositionForOne);
+    }
+
+    private static int findFirstPosition(int[] a, int start, int end, int target) {
+        int firstPosition = -1;
+
+        while(start <= end) {
+            int mid = (start + end)/2;
+
+            if(target == a[mid]) {
+                // Found target, update firstPosition
+                // and move to the left to find a smaller position by setting end=mid-1
+                firstPosition = mid;
+                end = mid-1;
+            } else if (target < a[mid]) {
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return firstPosition;
+    }
+
 }
