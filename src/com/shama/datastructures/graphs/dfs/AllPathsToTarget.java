e71a2c107448e8d0eb46a8b04ae4af0748195cbb
Shama Ugale <shama.ugale@gmail.com>
2022-06-06 02:01:23 +0530

    AllPathsToTarget dfs

---
+++ src/com/shama/datastructures/graphs/dfs/AllPathsToTarget.java
@@ -0,0 +1,29 @@
+package com.shama.datastructures.graphs.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPathsToTarget {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        path.add(0);
+        dfsSearch(graph, 0, res, path);
+
+        return res;
+    }
+
+    private void dfsSearch(int[][] graph, int node, List<List<Integer>> res, List<Integer> path) {
+        if (node == graph.length - 1) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+
+        for (int nextNode : graph[node]) {
+            path.add(nextNode);
+            dfsSearch(graph, nextNode, res, path);
+            path.remove(path.size() - 1); // as its a directed graph, in directed graph we rest it
+        }
+    }
+}
