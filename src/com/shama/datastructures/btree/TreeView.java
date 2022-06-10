56532903fc659dcf5ff4e6009cb3ef9867d2b6f5
Shama Ugale <shama.ugale@gmail.com>
2022-06-11 01:21:51 +0530

    adding recursive right and left view

--- src/com/shama/datastructures/btree/TreeView.java
+++ src/com/shama/datastructures/btree/TreeView.java
@@ -11,20 +11,21 @@ Output: [1,3,4]
  */
 public class TreeView {
 
-      public class TreeNode {
-          int val;
-          TreeNode left;
-          TreeNode right;
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-         TreeNode(int val) {
-             this.val = val;
-         }
-         TreeNode(int val, TreeNode left, TreeNode right) {
-             this.val = val;
-             this.left = left;
-              this.right = right;
-          }
-      }
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> rightView = new ArrayList<>();
@@ -70,23 +71,40 @@ public class TreeView {
         return leftView;
     }
 
-        public List<Integer> rightSideViewRecursive(TreeNode root) {
-            List<Integer> result = new ArrayList<Integer>();
-            rightView(root, result, 0);
-            return result;
-        }
-
-        public void rightView(TreeNode curr, List<Integer> result, int currDepth){
-            if(curr == null){
-                return;
-            }
-            if(currDepth == result.size()){
-                result.add(curr.val);
-            }
-
-            rightView(curr.right, result, currDepth + 1);
-            rightView(curr.left, result, currDepth + 1);
-
-        }
+    public List<Integer> rightSideViewRecursive(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
     }
+
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth) {
+        if (curr == null) {
+            return;
+        }
+        if (currDepth == result.size()) {
+            result.add(curr.val);
+        }
+
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+
+    }
+
+    public List<Integer> leftSideViewRecursive(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+
+    public void leftView(TreeNode curr, List<Integer> result, int currDepth) {
+        if (curr == null) {
+            return;
+        }
+        if (currDepth == result.size()) {
+            result.add(curr.val);
+        }
+        leftView(curr.left, result, currDepth + 1);
+        leftView(curr.right, result, currDepth + 1);
+    }
+}
 
