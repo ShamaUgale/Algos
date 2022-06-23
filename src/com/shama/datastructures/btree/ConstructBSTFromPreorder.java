0c30c07d030315dae996538d047eabe4de8e2573
Shama Ugale <shama.ugale@gmail.com>
2022-06-24 00:35:08 +0530

    bstFromPreorder

---
+++ src/com/shama/datastructures/btree/ConstructBSTFromPreorder.java
@@ -0,0 +1,15 @@
+package com.shama.datastructures.btree;
+
+public class ConstructBSTFromPreorder {
+    public TreeNode bstFromPreorder(int[] A) {
+        return bstFromPreorder(A, Integer.MAX_VALUE, new int[]{0});
+    }
+
+    public TreeNode bstFromPreorder(int[] A, int bound, int[] i) {
+        if (i[0] == A.length || A[i[0]] > bound) return null;
+        TreeNode root = new TreeNode(A[i[0]++]);
+        root.left = bstFromPreorder(A, root.val, i);
+        root.right = bstFromPreorder(A, bound, i);
+        return root;
+    }
+}
